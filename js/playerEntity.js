3917f16bcfe4aa27aed0f4c72ab5d308be7b236c
tkuczyn <tkuczyn@gmail.com>
2018-07-13 22:18:20 -0400

    New Feature: Added support for character to sprint (holding down L key while moving)

--- js/playerEntity.js
+++ js/playerEntity.js
@@ -8,10 +8,12 @@ app.PlayerEntity = function(){
     app.Entity.call(this,x,y,vertices,col,mass,type);
 
     this.applyCollisions = false;
-	this.terminalRightVel = 2.5;
-	this.terminalLeftVel = -2.5;
-	this.terminalUpVel = -2.5;
-	this.terminalDownVel = 2.5;
+	  this.terminalRightVel = 2.5;
+	  this.terminalLeftVel = -2.5;
+	  this.terminalUpVel = -2.5;
+	  this.terminalDownVel = 2.5;
+
+    this.sprint = false;
   };
 
   PlayerEntity.prototype = Object.create(app.Entity.prototype);
@@ -20,8 +22,8 @@ app.PlayerEntity = function(){
 
   p.moveLeft = function(force){
   	if(this.velocity[0] > 0) {this.velocity = vec2.fromValues(0, this.velocity[1]);}
-	if(this.velocity[0] < this.terminalLeftVel){
-		this.velocity = vec2.fromValues(this.terminalLeftVel, this.velocity[1]);
+	if(this.velocity[0] < this.getTerminalLeftVel()){
+		this.velocity = vec2.fromValues(this.getTerminalLeftVel(), this.velocity[1]);
 	} else {
 		this.applyWorldForces(force);
 	}
@@ -29,8 +31,8 @@ app.PlayerEntity = function(){
 
   p.moveRight = function(force){
   	if(this.velocity[0] < 0) {this.velocity = vec2.fromValues(0, this.velocity[1]);}
-	if(this.velocity[0] > this.terminalRightVel){
-		this.velocity = vec2.fromValues(this.terminalRightVel, this.velocity[1]);
+	if(this.velocity[0] > this.getTerminalRightVel()){
+		this.velocity = vec2.fromValues(this.getTerminalRightVel(), this.velocity[1]);
 	} else {
 		this.applyWorldForces(force);
 	}
@@ -42,8 +44,8 @@ app.PlayerEntity = function(){
 
   p.moveDown = function(force){
     if(this.velocity[1] < 0) {this.velocity = vec2.fromValues(this.velocity[0], 0);}
-	if(this.velocity[1] > this.terminalDownVel){
-		this.velocity = vec2.fromValues(this.velocity[0], this.terminalDownVel);
+	if(this.velocity[1] > this.getTerminalDownVel()){
+		this.velocity = vec2.fromValues(this.velocity[0], this.getTerminalDownVel());
 	} else {
 		this.applyWorldForces(force);
 	}
@@ -51,8 +53,8 @@ app.PlayerEntity = function(){
 
   p.moveUp = function(force){
     if(this.velocity[1] > 0) {this.velocity = vec2.fromValues(this.velocity[0], 0);}
-  	if(this.velocity[1] < this.terminalUpVel){
-  		this.velocity = vec2.fromValues(this.velocity[0], this.terminalUpVel);
+  	if(this.velocity[1] < this.getTerminalUpVel()){
+  		this.velocity = vec2.fromValues(this.velocity[0], this.getTerminalUpVel());
   	} else {
   		this.applyWorldForces(force);
   	}
@@ -60,6 +62,30 @@ app.PlayerEntity = function(){
 
   p.stopUpDown = function(){
     this.velocity[1] = 0;
+  }
+
+  p.startSprint = function(){
+    this.sprint = true;
+  }
+
+  p.stopSprint = function(){
+    this.sprint = false;
+  }
+
+  p.getTerminalRightVel = function(){
+    return this.sprint ? 2 * this.terminalRightVel : this.terminalRightVel;
+  }
+
+  p.getTerminalLeftVel = function(){
+    return this.sprint ? 2 * this.terminalLeftVel : this.terminalLeftVel;
+  }
+
+  p.getTerminalDownVel = function(){
+    return this.sprint ? 2 * this.terminalDownVel : this.terminalDownVel;
+  }
+
+  p.getTerminalUpVel = function(){
+    return this.sprint ? 2 * this.terminalUpVel : this.terminalUpVel;
   }
 
   return PlayerEntity;
